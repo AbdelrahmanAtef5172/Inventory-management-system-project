5f3ebf5798f2c8be81f1c0a9c755e67e06c6c0c6
AbdelrahmanAtef5172 <abdelrahmanatef426@gmail.com>
2025-10-20 09:09:03 +0300

    Update CustomerProductTest.java

--- src/customer/CustomerProductTest.java
+++ src/customer/CustomerProductTest.java
@@ -2,7 +2,7 @@ package customer;
 
 public class CustomerProductTest {
     public static void main(String[] args) {
-        CustomerProduct cp = new CustomerProduct("123-45-6789", "P001", java.time.LocalDate.of(2025, 6, 12));
+        CustomerProduct cp = new CustomerProduct("123-45-6789", "P0011", java.time.LocalDate.of(2025, 6, 12));
         cp.setPaid(true);
 
         System.out.println("Line Representation: " + cp.lineRepresentation());
