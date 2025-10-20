edc969d2874ccaaf4b4159ca6863308608fa2539
AbdelrahmanAtef5172 <abdelrahmanatef426@gmail.com>
2025-10-20 08:23:17 +0300

    Update CustomerProductDatabase.java

--- src/customer/CustomerProductDatabase.java
+++ src/customer/CustomerProductDatabase.java
@@ -1,13 +1,27 @@
 package customer;
-
-import java.util.ArrayList;
-
 import abstractClasses.DataBase;
-
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class CustomerProductDatabase extends DataBase<CustomerProduct> {
-    
-    
 
+    // CLASS CONSTRUCTOR
+    public CustomerProductDatabase(String fileName) {
+        super(fileName);}
 
+    // IMPLEMENTATION OF ABSTRACT METHOD FROM DATABASE
+    @Override
+    public CustomerProduct createRecordFrom(String line) {
+        String[] dataCustomerProduct = line.split(",");
+        String customerSSN = dataCustomerProduct[0];
+        String productID = dataCustomerProduct[1];
+        String dateString = dataCustomerProduct[2];
+        boolean paid = Boolean.parseBoolean(dataCustomerProduct[3]);
+        //SPECIFYING THE PATTERN
+        DateTimeFormatter date = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate purchaseDate = LocalDate.parse(dateString, date);
+        //CREATING NEW OBJECT
+        CustomerProduct record = new CustomerProduct(customerSSN, productID, purchaseDate);
+        record.setPaid(paid);
+        return record;}
 }
