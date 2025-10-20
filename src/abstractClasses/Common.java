69904e679438b186e7f9fbd4bb741a428a7697ac
AbdelrahmanAtef5172 <abdelrahmanatef426@gmail.com>
2025-10-20 07:10:35 +0300

    renaming file from recordable.java to common.java

---
+++ src/abstractClasses/Common.java
@@ -0,0 +1,9 @@
+package abstractClasses;
+
+public abstract class Common {
+    //ABSTRACT METHOD TO GET THE LINE REPRESENTATION OF OBJECT'S DATA
+    public abstract String lineRepresentation();
+    //ABSTRACT METHOD TO GET THE ID OF THE OBJECT
+    public abstract String getSearchKey();
+
+}
